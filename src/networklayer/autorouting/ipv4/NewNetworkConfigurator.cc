bb1920c0f14a2a803179a6e14705b939e8304d0d
Andras Varga <andras@omnetpp.org>
2012-01-05 23:18:09 +0100

    NewNetworkConfigurator /1

---
+++ src/networklayer/autorouting/ipv4/NewNetworkConfigurator.cc
@@ -0,0 +1,125 @@
+//
+// Copyright (C) 2011 Opensim Ltd
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program; if not, see <http://www.gnu.org/licenses/>.
+//
+
+#include <set>
+#include <algorithm>
+#include "IRoutingTable.h"
+#include "IInterfaceTable.h"
+#include "IPvXAddressResolver.h"
+#include "NewNetworkConfigurator.h"
+#include "InterfaceEntry.h"
+#include "IPv4InterfaceData.h"
+
+
+Define_Module(NewNetworkConfigurator);
+
+
+void NewNetworkConfigurator::initialize(int stage)
+{
+    if (stage==2)
+    {
+        cTopology topo("topo");
+        LinkInfoVector linkInfo;
+
+        // extract topology into the cTopology object, then fill in a LinkInfo[] vector
+        extractTopology(topo, linkInfo);
+    }
+}
+
+static cTopology::LinkOut *findLinkOut(cTopology::Node *node, int gateId)
+{
+	for (int i=0; i<node->getNumOutLinks(); i++)
+		if (node->getLinkOut(i)->getLocalGateId() == gateId)
+			return node->getLinkOut(i);
+	return NULL;
+}
+
+void NewNetworkConfigurator::extractTopology(cTopology& topo, LinkInfoVector& linkInfoVector)
+{
+    // extract topology
+    topo.extractByProperty("node");
+    EV << "cTopology found " << topo.getNumNodes() << " nodes\n";
+
+    std::set<InterfaceEntry*> interfacesSeen;
+    for (int i = 0; i < topo.getNumNodes(); i++)
+    {
+    	cModule *mod = topo.getNode(i)->getModule();
+    	IInterfaceTable *ift = IPvXAddressResolver().findInterfaceTableOf(mod);
+    	if (ift)
+    	{
+    		for (int j = 0; j < ift->getNumInterfaces(); j++)
+    		{
+    			InterfaceEntry *ie = ift->getInterface(j);
+    			if (interfacesSeen.count(ie) == 0)  // "not yet seen"
+    			{
+    				// store interface as belonging to a new network link
+			        linkInfoVector.push_back(LinkInfo());
+			        LinkInfo& linkInfo = linkInfoVector.back();
+			        linkInfo.interfaces.push_back(new InterfaceInfo(ie));
+    				interfacesSeen.insert(ie);
+
+    				// visit neighbor (and potentially the whole LAN, recursively)
+    				cTopology::LinkOut *linkOut = findLinkOut(topo.getNode(i), ie->getNodeOutputGateId());
+    				if (linkOut)
+    				{
+				        std::vector<cTopology::Node*> empty;
+				        visitNeighbor(linkOut, linkInfo, interfacesSeen, empty);
+    				}
+    			}
+    		}
+    	}
+    }
+}
+
+template<typename T>
+inline bool contains(std::vector<T> v, T& a) {return std::find(v.begin(), v.end(), a) != v.end();}
+
+void NewNetworkConfigurator::visitNeighbor(cTopology::LinkOut *linkOut, LinkInfo& linkInfo,
+		std::set<InterfaceEntry*>& interfacesSeen, std::vector<cTopology::Node*>& deviceNodesVisited)
+{
+    cModule *neighborMod = linkOut->getRemoteNode()->getModule();
+    int neighborInputGateId = linkOut->getRemoteGateId();
+    IInterfaceTable *neighborIft = IPvXAddressResolver().findInterfaceTableOf(neighborMod);
+    if (neighborIft)
+    {
+        // neighbor is a host or router, just add the interface
+        InterfaceEntry *neighborIe = neighborIft->getInterfaceByNodeInputGateId(neighborInputGateId);
+        linkInfo.interfaces.push_back(new InterfaceInfo(neighborIe));
+        interfacesSeen.insert(neighborIe);
+    }
+    else
+    {
+        // assume that neighbor is an L2 or L1 device (bus/hub/switch/bridge/access point/etc); visit all its output links
+        cTopology::Node *deviceNode = linkOut->getRemoteNode();
+        if (contains(deviceNodesVisited, deviceNode))
+        {
+        	deviceNodesVisited.push_back(deviceNode);
+        	for (int i = 0; i < deviceNode->getNumOutLinks(); i++)
+        	{
+        		cTopology::LinkOut *deviceLinkOut = deviceNode->getLinkOut(i);
+        		visitNeighbor(deviceLinkOut, linkInfo, interfacesSeen, deviceNodesVisited);
+        	}
+        }
+    }
+}
+
+
+void NewNetworkConfigurator::handleMessage(cMessage *msg)
+{
+    error("this module doesn't handle messages, it runs only in initialize()");
+}
+
---
+++ src/networklayer/autorouting/ipv4/NewNetworkConfigurator.h
@@ -0,0 +1,57 @@
+//
+// Copyright (C) 2011 Opensim Ltd
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program; if not, see <http://www.gnu.org/licenses/>.
+//
+
+#ifndef __INET_NEWNETWORKCONFIGURATOR_H
+#define __INET_NEWNETWORKCONFIGURATOR_H
+
+#include <omnetpp.h>
+#include "INETDefs.h"
+#include "IPv4Address.h"
+
+class IInterfaceTable;
+class IRoutingTable;
+
+
+/**
+ * Configures IPv4 addresses for a network,
+ *
+ * For more info please see the NED file.
+ */
+class INET_API NewNetworkConfigurator : public cSimpleModule
+{
+  protected:
+    struct InterfaceInfo {
+    	InterfaceInfo(InterfaceEntry *ie) {entry = ie;}
+    	InterfaceEntry *entry;
+    };
+    struct LinkInfo {
+    	std::vector<InterfaceInfo*> interfaces;
+    	~LinkInfo() {/*TODO delete interfaceinfos*/}
+    };
+    typedef std::vector<LinkInfo> LinkInfoVector;
+
+  protected:
+    virtual int numInitStages() const  {return 3;}
+    virtual void initialize(int stage);
+    virtual void handleMessage(cMessage *msg);
+
+    virtual void extractTopology(cTopology& topo, LinkInfoVector& linkInfo);
+    virtual void visitNeighbor(cTopology::LinkOut *linkOut, LinkInfo& linkInfo, std::set<InterfaceEntry*>& interfacesSeen, std::vector<cTopology::Node*>& nodesVisited);
+};
+
+#endif
+
